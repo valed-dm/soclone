9284be4c53a0e7b566392c45644ed7917300cb24
valed-dm <dmvaled@gmail.com>
2024-02-20 18:57:43 +0300

    Authorization callback URL 'callback/' replaced for 'accounts/'
    in GitHub OAuth app settings

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("users/", include("soclone.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("callback/", include("allauth.urls")),
+    #
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
