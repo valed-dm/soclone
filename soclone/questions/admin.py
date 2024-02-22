84076afacae9a083ca9749bf2ed2aa16b3570b86
valed-dm <dmvaled@gmail.com>
2024-02-23 02:19:25 +0300

    new models registered to questions admin.py

--- soclone/questions/admin.py
+++ soclone/questions/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
+from .models import Answer
+from .models import AnswerVote
 from .models import Question
+from .models import QuestionView
+from .models import QuestionVote
 from .models import Tag
 
+admin.site.register(Answer)
+admin.site.register(AnswerVote)
 admin.site.register(Question)
+admin.site.register(QuestionView)
+admin.site.register(QuestionVote)
 admin.site.register(Tag)
