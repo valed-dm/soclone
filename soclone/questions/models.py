171f2f9ae28215961d21edcb6f38eed137e5f172
valed-dm <dmvaled@gmail.com>
2024-02-23 02:12:56 +0300

    questions db models expanded for voting facilities

--- soclone/questions/models.py
+++ soclone/questions/models.py
@@ -2,6 +2,8 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 
+from soclone.users.models import User
+
 validators: list = [
     MinLengthValidator(
         limit_value=20, message="Description must be 20 characters or more."
@@ -9,20 +11,48 @@ validators: list = [
 ]
 
 
-class Tag(models.Model):
+class TimestampMixin(models.Model):
+    """Timestamp mixin."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserMixin(models.Model):
+    """User mixin."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class AppraisalMixin(UserMixin):
+    """Appraisal mixin."""
+
+    is_useful = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Tag(UserMixin, TimestampMixin):
     """Tag model."""
 
     name = models.CharField(max_length=50, unique=True)
-    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return f"{self.name!r}"
 
 
-class Question(models.Model):
+class Question(UserMixin, TimestampMixin):
     """Question model."""
 
     title = models.CharField(max_length=200, unique=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     problem = models.TextField(
         help_text=(
             "What are the details of your problem?\n"
@@ -42,8 +72,72 @@ class Question(models.Model):
         unique=True,
         validators=validators,
     )
-    pub_date = models.DateTimeField("date published")
-    tags = models.ManyToManyField(Tag, related_name="questions", blank=True)
 
     def __str__(self):
-        return f"{self.title!r}"
+        return f"{self.title[:20]!r}.."
+
+
+class Answer(UserMixin, TimestampMixin):
+    """Answer model."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.TextField(
+        help_text=(
+            "Give a brief description of underlying conditions."
+            "Propose some clues, methods or attach code samples."
+            "Minimum 20 characters."
+        ),
+        unique=True,
+        validators=validators,
+    )
+
+    def __str__(self):
+        return f"reply to {self.question.title[:20]!r}.."
+
+
+class QuestionVote(AppraisalMixin, TimestampMixin):
+    """Question vote model."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["question", "user"], name="unique_question_vote"
+            )
+        ]
+
+    def __str__(self):
+        return f"vote for question {self.question.title[:20]!r}.."
+
+
+class AnswerVote(AppraisalMixin, TimestampMixin):
+    """Answer vote model."""
+
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["answer", "user"], name="unique_answer_vote"
+            )
+        ]
+
+    def __str__(self):
+        return f"vote for answer to {self.answer.question.title[:20]!r}.."
+
+
+class QuestionView(UserMixin, TimestampMixin):
+    """Question view model."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["question", "user"], name="unique_question_user_view"
+            )
+        ]
+
+    def __str__(self):
+        return f"question {self.question.title[:20]!r} user {self.user.email} view"
