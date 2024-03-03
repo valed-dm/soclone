a8b09d5bd44b29cfc5658d3dfeb061797ed53935
valed-dm <dmvaled@gmail.com>
2024-03-03 13:43:54 +0300

    QuestionForm for CreateQuestionView added

---
+++ soclone/questions/forms.py
@@ -0,0 +1,28 @@
+"""Forms for the questions app."""
+from django import forms
+
+from .models import Question
+from .models import Tag
+
+
+class QuestionForm(forms.ModelForm):
+    """Form for creating Question"""
+
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Enter question title", "class": "form-control"}
+        )
+    )
+
+    problem = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control"}))
+
+    effort = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control"}))
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all().order_by("name"),
+        widget=forms.SelectMultiple(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = Question
+        fields = ["title", "problem", "effort", "tags"]
