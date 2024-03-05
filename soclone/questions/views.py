d62bea7336123e36d0275688ce3051b481c5ab46
valed-dm <dmvaled@gmail.com>
2024-03-05 13:45:49 +0300

    QuestionFullView created

--- soclone/questions/views.py
+++ soclone/questions/views.py
@@ -1,4 +1,6 @@
 """Views for the questions app."""
+from datetime import timedelta
+
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
@@ -6,11 +8,17 @@ from django.db.models import F
 from django.db.models import OuterRef
 from django.db.models import Subquery
 from django.db.models.query import QuerySet
+from django.http import HttpResponseForbidden
+from django.urls import reverse
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.views import View
 from django.views import generic
+from django.views.generic.detail import SingleObjectMixin
 
+from soclone.questions.forms import AnswerForm
 from soclone.questions.forms import QuestionForm
+from soclone.questions.models import Answer
 from soclone.questions.models import Question
 from soclone.questions.models import QuestionView
 from soclone.questions.models import QuestionVote
@@ -20,7 +28,7 @@ from soclone.questions.models import Tag
 class QuestionsView(generic.ListView):
     """Renders questions table."""
 
-    template_name: str = "questions/questions.html"
+    template_name: str = "questions/question_list.html"
     context_object_name: str = "questions_list"
 
     def get_queryset(self) -> QuerySet[Question]:
@@ -49,13 +57,92 @@ class QuestionsView(generic.ListView):
                     .annotate(qty=Count("question"))
                     .values("qty")
                 ),
-                asked=(timezone.now() - F("created_at")),
+                posed=(timezone.now() - F("created_at")),
             )
             .filter(created_at__lte=timezone.now())
             .order_by("-created_at")[:10]
         )
 
         return latest_questions
+
+
+class QuestionDetailView(generic.DetailView):
+    """Renders question data."""
+
+    model = Question
+    context_object_name = "question"
+
+    def get_context_data(self, **kwargs):
+        """Enrich context data with statistics and answer form."""
+        context = super().get_context_data(**kwargs)
+        q = context["question"]
+        cat = q.created_at
+        uat = q.updated_at
+        views: int = QuestionView.objects.filter(question=q.id).count()
+        answers = Answer.objects.filter(question=q.id).select_related("user").all()
+        context["posed"] = timezone.now() - cat
+        context["modified"] = (
+            (timezone.now() - uat) if (uat - cat) > timedelta(seconds=1) else None
+        )
+        context["views"] = views
+        context["answers"] = answers
+        context["answers_qty"] = len(answers) if answers else 0
+        context["answer_form"] = AnswerForm
+        return context
+
+
+class QuestionAnswerFormView(SingleObjectMixin, generic.FormView):
+    """Renders answer form for question."""
+
+    template_name = "questions/question_detail.html"
+    form_class = AnswerForm
+    model = Answer
+
+    def post(self, request, *args, **kwargs):
+        """Enable post request for question by authorized users."""
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        answer = Answer(
+            user=self.request.user,
+            question=Question.objects.get(id=self.kwargs.get("pk")),
+            body=self.request.POST["body"],
+        )
+        answer.save()
+        messages.success(self.request, "The answer was added successfully.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("questions:question", kwargs={"pk": self.kwargs.get("pk")})
+
+
+class QuestionFullView(View):
+    """Renders full question view"""
+
+    def get(self, request, *args, **kwargs):
+        view = QuestionDetailView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = QuestionAnswerFormView.as_view()
+        return view(request, *args, **kwargs)
+
+
+class QuestionCreateView(LoginRequiredMixin, generic.CreateView):
+    """Creates a question."""
+
+    # CreateView class uses the question_form.html template from the templates/questions
+    # with the following naming convention: model_form.html
+    model = Question
+    form_class = QuestionForm
+    success_url = reverse_lazy("questions:questions")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The question was created successfully.")
+        return super().form_valid(form)
 
 
 class TagsView(generic.ListView):
@@ -70,18 +157,3 @@ class TagsView(generic.ListView):
         return Tag.objects.filter(created_at__lte=timezone.now()).order_by(
             "-created_at"
         )[:10]
-
-
-class CreateQuestionView(LoginRequiredMixin, generic.CreateView):
-    """Creates a question."""
-
-    # CreateView class uses the question_form.html template from the templates/questions
-    # with the following naming convention: model_form.html
-    model = Question
-    form_class = QuestionForm
-    success_url = reverse_lazy("questions:questions")
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        messages.success(self.request, "The question was created successfully.")
-        return super().form_valid(form)
