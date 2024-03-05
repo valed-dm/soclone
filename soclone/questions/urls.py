cd6c94085c8cf62a300ed9143059d8dad8947d61
valed-dm <dmvaled@gmail.com>
2024-03-05 13:48:06 +0300

    'question' path added to 'questions' app

--- soclone/questions/urls.py
+++ soclone/questions/urls.py
@@ -7,6 +7,7 @@ app_name = "questions"
 urlpatterns = [
     # ex: /questions/
     path("", views.QuestionsView.as_view(), name="questions"),
-    path("create/", views.CreateQuestionView.as_view(), name="question_create"),
+    path("<int:pk>/", views.QuestionFullView.as_view(), name="question"),
+    path("create/", views.QuestionCreateView.as_view(), name="question_create"),
     path("tags/", views.TagsView.as_view(), name="tags"),
 ]
