8766f2adddfac58fe294e7148adb3b29b6766b82
valed-dm <dmvaled@gmail.com>
2024-03-10 19:50:58 +0300

    QuestionViewMiddleware AnonymousUser case illustrated

--- soclone/questions/middleware.py
+++ soclone/questions/middleware.py
@@ -28,10 +28,13 @@ class QuestionViewMiddleware:
         When user is anonymous ip address serves as user identity.
         """
         response = self.get_response(request)
+
         path: str = request.path
         rx = re.compile(r"^/questions/(?P<pk>\d+)/$")
+
         if rx.match(path):
             # AnonymousUser object has id=None and pk=None
+            # AnonymousUser is replaced by None
             user = request.user if request.user.is_authenticated else None
             pk = int(re.search("\\d+", path)[0])
             ip_address: str = ipaddress(request)
@@ -39,7 +42,7 @@ class QuestionViewMiddleware:
             question = Question.objects.get(id=pk)
             # Keeps unique user-question-ip-date data for possible future analysis
             QuestionsViewsIP.objects.update_or_create(
-                # AnonymousUser object is erroneous here and is replaced by None
+                # AnonymousUser object would be erroneous here
                 user=user,
                 question=question,
                 ip_address=ip_address,
